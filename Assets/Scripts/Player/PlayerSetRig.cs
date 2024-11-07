4f164a37318df5a61c93d4b1c50b4b88c004fe1d
KunRalf <rshumaher1996@gmail.com>
2024-11-08 00:39:20 +0300

    add on serv sync

--- Assets/Scripts/Player/PlayerSetRig.cs
+++ Assets/Scripts/Player/PlayerSetRig.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
+using UnityEngine.Rendering;
+using Random = UnityEngine.Random;
 
 namespace Player
 {
@@ -17,7 +19,8 @@ namespace Player
     public class PlayerSetRig : NetworkBehaviour
     {
         [SerializeField] private RigBuilder _rig;
-        [SerializeField] private Transform _target;
+        [SyncVar(hook = nameof(SetAimRig))][SerializeField] private GameObject _target;
+        [SyncVar(hook = nameof(SetAim))][SerializeField] private uint _targetId;
         [SerializeField] private GameObject _aimLook;
         [Header("BodyAim")] 
         [SerializeField] private MultiAimConstraint _spineMain;
@@ -38,33 +41,63 @@ namespace Player
 
         public override void OnStartClient()
         {
-            if(!isOwned) return;
-            // var mainCam = Camera.main;
-            // Transform target = null;
-            // if (mainCam != null)
-            // {
-            //     target = mainCam.transform.GetChild(0);
-            //     if(target != null)
-            //     {
-            //         GameObject aimLook = Instantiate(_aimLook, target);
-            //         NetworkServer.Spawn(aimLook,connectionToClient);
-            //         _target = aimLook.transform;
-            //     }
-            // }
-            var newSourceArray = new WeightedTransformArray { new WeightedTransform(_target, 1f) };
+            Debug.Log(_targetId);
+            base.OnStartClient();
+            if (!isOwned)return;
+            CmdCreateAimLook();
+        }
+
+        [Command]
+        private void CmdCreateAimLook()
+        {
+            var aimLook = Instantiate(_aimLook);
+            
+            NetworkServer.Spawn(aimLook, connectionToClient);
+            RPCSetTarget(aimLook.GetComponent<NetworkIdentity>().netId);
+        }  
+        
+        private void SetAimRig(GameObject o, GameObject n)
+        {
+            var newSourceArray = new WeightedTransformArray { new WeightedTransform(_target.transform, 1f) };
             _spineMain.data.sourceObjects = newSourceArray;
             _spineSub.data.sourceObjects = newSourceArray;
             _head.data.sourceObjects = newSourceArray;
             _weapon.data.sourceObjects = newSourceArray;
             
             _rig.Build();
+        }  
+        private void SetAim(uint o, uint n)
+        {
+            // var newSourceArray = new WeightedTransformArray { new WeightedTransform(_target.transform, 1f) };
+            // _spineMain.data.sourceObjects = newSourceArray;
+            // _spineSub.data.sourceObjects = newSourceArray;
+            // _head.data.sourceObjects = newSourceArray;
+            // _weapon.data.sourceObjects = newSourceArray;
+            //
+            // _rig.Build();
+       
         }
 
-        private void Start()
+        [ClientRpc]
+        private void RPCSetTarget(uint id)
         {
-          
+            _targetId =id;
         }
         
+        private void Update()
+        {
+            if (!isOwned) return;
+            if(_target == null) return;
+            Vector2 screenPoint = new Vector2(Screen.width / 2, Screen.height / 2);
+            // Определяем позицию прицела в 3D-пространстве
+            Ray ray = Camera.main.ScreenPointToRay(screenPoint);
+
+            // Определяем дистанцию для точки в 3D-пространстве, например, 10 единиц вперед
+            float distance = 10f;
+
+            // Обновляем позицию TargetPoint на этой линии
+            _target.transform.position = ray.origin + ray.direction * distance;
+        }
 
         public void SetHandsOnWeapon(Transform lHand, Transform rHand)
         {
